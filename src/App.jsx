2ad2a6fa07ba359dff6b32f8603e3da91ddbdd84
Rafael Uribe <Rafael.Uribe.2080@gmail.com>
2023-06-02 20:52:59 -0400

    Remove Activity hook from App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import Header from "./Header.jsx";
 import ActivityGroup from "./components/ActivityGroup/ActivityGroup.jsx";
-import useActivity from "./hooks/useActivity.js";
 
 const App = () => {
-  const { loading, unarchived, archived } = useActivity();
   return (
     <div className="container">
       <Header />
