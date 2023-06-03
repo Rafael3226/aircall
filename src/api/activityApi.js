6cbcee7009c7d1a8fce761146255c5dd8d53f969
Rafael Uribe <Rafael.Uribe.2080@gmail.com>
2023-06-02 20:52:33 -0400

    Get Activities Api

--- src/api/activityApi.js
+++ src/api/activityApi.js
@@ -2,10 +2,4 @@ import axios from "axios";
 
 const API_URL = "https://cerulean-marlin-wig.cyclic.app";
 
-const getActivities = () => axios.get(`${API_URL}/activities`);
-
-const activityApi = {
-  getActivities,
-};
-
-export default activityApi;
+export const getActivities = () => axios.get(`${API_URL}/activities`);
