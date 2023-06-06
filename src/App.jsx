ef52010cb2146907bc5cdf2902fbf93c1658305f
Rafael Uribe <Rafael.Uribe.2080@gmail.com>
2023-06-06 03:04:07 -0400

    React router dom implement

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+import { Suspense } from "react";
+import { Route, HashRouter, Routes } from "react-router-dom";
+
 import Header from "./Header.jsx";
-import ActivityGroup from "./components/ActivityGroup/ActivityGroup.jsx";
+import Loading from "./components/Loading";
+import { lazy } from "react";
+
+const ActivitiesPage = lazy(() => import("./pages/ActivitiesPage.jsx"));
 
 const App = () => {
   return (
     <div className="container">
       <Header />
       <div className="container-view">
-        <ActivityGroup groups={[1, 2, 3]} />
+        <HashRouter>
+          <Suspense fallback={<Loading />}>
+            <Routes>
+              <Route path="*" element={<ActivitiesPage />} />
+            </Routes>
+          </Suspense>
+        </HashRouter>
       </div>
     </div>
   );
---
+++ src/pages/ActivitiesPage.jsx
@@ -0,0 +1,11 @@
+import ActivityGroup from "../components/ActivityGroup";
+
+const ActivitiesPage = () => {
+  return (
+    <div>
+      <ActivityGroup groups={[1, 2, 3]} />
+    </div>
+  );
+};
+
+export default ActivitiesPage;
