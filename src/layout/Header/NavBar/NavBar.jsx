cf12710f3f8d9ee33438f91827f52d64f455ba49
Rafael Uribe <Rafael.Uribe.2080@gmail.com>
2023-06-07 09:28:48 -0400

    Remove archive link if not needed

--- src/layout/Header/NavBar/NavBar.jsx
+++ src/layout/Header/NavBar/NavBar.jsx
@@ -1,10 +1,15 @@
+import { useSelector } from "react-redux";
 import { LinkContainer, Link } from "./NavBar.styles";
+import { selectArchived } from "../../../store/activities";
 
 const NavBar = () => {
+  const archived = useSelector(selectArchived);
+  const showArchivedLink = archived.length > 0;
+
   return (
     <LinkContainer>
       <Link to="/">Activity</Link>
-      <Link to="/archived">Archived</Link>
+      {showArchivedLink && <Link to="/archived">Archived</Link>}
     </LinkContainer>
   );
 };
