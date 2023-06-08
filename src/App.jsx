851c317001068a30e2ca6ef6d0df8467cb7b5053
Rafael Uribe <Rafael.Uribe.2080@gmail.com>
2023-06-08 04:47:39 -0400

    Add vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,10 @@ import Header from "./layout/Header";
 import Loading from "./components/Loading";
 import { lazy } from "react";
 import { useSelector } from "react-redux";
-import ActivityDetailsPage from "./pages/ActivityDetailsPage";
 import { selectLoading } from "./store/loading/selectors";
+import { Analytics } from "@vercel/analytics/react";
 
+const ActivityDetailsPage = lazy(() => import("./pages/ActivityDetailsPage"));
 const ActivityPage = lazy(() => import("./pages/ActivityPage.jsx"));
 const ArchivedActivityPage = lazy(() =>
   import("./pages/ArchivedActivityPage.jsx")
@@ -17,21 +18,24 @@ const App = () => {
   const isLoading = useSelector(selectLoading);
 
   return (
-    <div className="container">
-      <HashRouter>
-        <Header />
-        <div className="container-view">
-          {isLoading && <Loading />}
-          <Suspense fallback={<Loading />}>
-            <Routes>
-              <Route path="/details" element={<ActivityDetailsPage />} />
-              <Route path="/archived" element={<ArchivedActivityPage />} />
-              <Route path="*" element={<ActivityPage />} />
-            </Routes>
-          </Suspense>
-        </div>
-      </HashRouter>
-    </div>
+    <>
+      <div className="container">
+        <HashRouter>
+          <Header />
+          <div className="container-view">
+            {isLoading && <Loading />}
+            <Suspense fallback={<Loading />}>
+              <Routes>
+                <Route path="/details" element={<ActivityDetailsPage />} />
+                <Route path="/archived" element={<ArchivedActivityPage />} />
+                <Route path="*" element={<ActivityPage />} />
+              </Routes>
+            </Suspense>
+          </div>
+        </HashRouter>
+      </div>
+      <Analytics />
+    </>
   );
 };
 
